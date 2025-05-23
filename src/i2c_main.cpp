b4b36393589d469e9f21d02dc7e91ef911997a3d
Benjamin lightshade <benjamintql@gmail.com>
2025-05-23 15:27:57 +0800

    Added Cmake requirements for using internal and external libraries

---
+++ src/i2c_main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+
+#include "test_lib.h"
+#include "i2c/i2c.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    
+    
+
+    test_func();
+
+    
+    return 0;
+}
---
+++ src/test_lib.cpp
@@ -0,0 +1,7 @@
+#include "test_lib.h"
+#include <iostream>
+
+
+void test_func() {
+    std::cout << "Test function called!" << std::endl;
+}
